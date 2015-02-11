c6e3824f3cb9da8f645ae4b8cc076e3906d382de
mioxin <mioxin@ya.ru>
2015-02-12 00:34:38 +0600

    init

--- app/src/main/java/com/example/mmm/handler_mess/MainActivity.java
+++ app/src/main/java/com/example/mmm/handler_mess/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.mmm.handler_mess;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    final String LOG_TAG = "myLogs";
+    final int STATUS_NONE = 0; // нет подключения
+    final int STATUS_CONNECTING = 1; // подключаемся
+    final int STATUS_CONNECTED = 2; // подключено
+    Handler h;
+    TextView tvStatus;
+    ProgressBar pbConnect;
+    Button btnConnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
